705645d70c71842ad0cb58de9d731be3f8367d49
Saidbeg-I <saido1996@mail.ru>
2023-01-12 21:22:34 +1200

    Initial Commit

---
+++ HellowWorldApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HellowWorldApp
+//
+//  Created by 1 on 12.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
